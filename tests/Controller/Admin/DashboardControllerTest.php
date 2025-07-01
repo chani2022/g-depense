8f9a1c2172beb98cebe6d577ad3af537116e52c5
chani <chaniandrianarinaivo@gmail.com>
2025-07-01 16:51:07 +0100

    test: echec lorsqu'on accede au dashboard

---
+++ tests/Controller/Admin/DashboardControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Controller\Admin;
+
+use App\Tests\Trait\LoadFixtureTrait;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class DashboardControllerTest extends WebTestCase
+{
+    use RefreshDatabaseTrait;
+    use LoadFixtureTrait;
+
+    /** @var MockObject&KernelBrowser&null*/
+    private $client;
+
+    private array|null $all_fixtures;
+
+    protected function setUp(): void
+    {
+        $this->client = $this->createClient();
+        $this->all_fixtures = $this->getFixtures();
+    }
+
+    public function testPageIndexExist(): void
+    {
+        $userLogged = $this->all_fixtures['user_credentials_ok'];
+        $this->client->loginUser($userLogged);
+        $this->client->request('GET', '/dashboard');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Dashboard');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->client = null;
+    }
+}
