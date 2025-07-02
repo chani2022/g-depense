a47f062c6496782b66dec3d680e432b4aca90f11
chani <chaniandrianarinaivo@gmail.com>
2025-07-02 10:28:11 +0100

    chrone: ajout de role hierarchy security.yaml

---
+++ tests/Controller/Admin/UserCrudControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Controller\Admin;
+
+use App\Controller\Admin\DashboardController;
+use App\Controller\Admin\UserCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Test\AbstractCrudTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+final class CategoryCrudControllerTest extends AbstractCrudTestCase
+{
+    use RefreshDatabaseTrait;
+
+    protected function getControllerFqcn(): string
+    {
+        return UserCrudController::class;
+    }
+
+    protected function getDashboardFqcn(): string
+    {
+        return DashboardController::class;
+    }
+
+    public function testIndexPageUser(): void
+    {
+        // this examples doesn't use security; in your application you may
+        // need to ensure that the user is logged before the test
+        $this->client->request("GET", $this->generateIndexUrl());
+        static::assertResponseIsSuccessful();
+    }
+}
