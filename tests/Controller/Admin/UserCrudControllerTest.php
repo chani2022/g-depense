b2c3da1eb409740ddebc7f1093e79cfba39ae96a
chani <chaniandrianarinaivo@gmail.com>
2025-07-04 09:27:50 +0100

    fix(test): resolution du conflit dans dashbard controller test

--- tests/Controller/Admin/UserCrudControllerTest.php
+++ tests/Controller/Admin/UserCrudControllerTest.php
@@ -4,12 +4,15 @@ namespace App\Tests\Controller\Admin;
 
 use App\Controller\Admin\DashboardController;
 use App\Controller\Admin\UserCrudController;
+use App\Tests\Trait\LoadFixtureTrait;
 use EasyCorp\Bundle\EasyAdminBundle\Test\AbstractCrudTestCase;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use App\Entity\User;
 
 final class UserCrudControllerTest extends AbstractCrudTestCase
 {
     use RefreshDatabaseTrait;
+    use LoadFixtureTrait;
 
     protected function getControllerFqcn(): string
     {
@@ -23,8 +26,11 @@ final class UserCrudControllerTest extends AbstractCrudTestCase
 
     public function testIndexPageUser(): void
     {
+        /** @var User */
+        $authenticatedUser = $this->getFixtures()['user_admin'];
         // this examples doesn't use security; in your application you may
         // need to ensure that the user is logged before the test
+        $this->client->loginUser($authenticatedUser);
         $this->client->request("GET", $this->generateIndexUrl());
         static::assertResponseIsSuccessful();
     }
