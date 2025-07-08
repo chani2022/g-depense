e0967e413eb7996af86c831fd9b81b9de93c8132
chani <chaniandrianarinaivo@gmail.com>
2025-07-08 13:38:29 +0100

    test: echec lorsqu'un utilisateur anonyme tente d'acceder à la page profil

--- tests/Controller/Admin/UserCrudControllerTest.php
+++ tests/Controller/Admin/UserCrudControllerTest.php
@@ -55,15 +55,6 @@ final class UserCrudControllerTest extends AbstractCrudTestCase
             static::assertResponseStatusCodeSame(403);
         }
     }
-
-    public function testProfilePageExist(): void
-    {
-        $authenticatedUser = $this->getFixtures()['user_credentials_ok'];
-        $this->client->loginUser($authenticatedUser);
-
-        $this->client->request('GET', '/profil');
-        $this->assertResponseIsSuccessful();
-    }
     /**
      * @return array<array{string, string}>
      */
---
+++ tests/Controller/ProfilControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Tests\Trait\LoadFixtureTrait;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+
+class ProfilControllerTest extends WebTestCase
+{
+    use RefreshDatabaseTrait;
+    use LoadFixtureTrait;
+
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = $this->createClient();
+    }
+
+    public function testPageProfilNotAccessIfUserIsAnonymous(): void
+    {
+        $this->client->request('GET', '/profil');
+        $this->assertResponseIsSuccessful();
+    }
+    public function testProfilePageExist(): void
+    {
+        $authenticatedUser = $this->getFixtures()['user_credentials_ok'];
+        $this->client->loginUser($authenticatedUser);
+
+        $this->client->request('GET', '/profil');
+        $this->assertResponseIsSuccessful();
+    }
+
+    // public function
+}
