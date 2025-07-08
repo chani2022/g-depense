2b7d3dc13e911d04286dff472cd8b6695615b1f4
chani <chaniandrianarinaivo@gmail.com>
2025-07-08 11:17:39 +0100

    feat: ajout d'action profil dans user crud

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,9 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminAction;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_ADMIN')]
@@ -24,5 +30,14 @@ class UserCrudController extends AbstractCrudController
             TextField::new('prenom', 'Prénom'),
             TextField::new('username', 'Nom d\'utilisateur'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $profilAction = Action::new('profil', 'Profil', 'fa fa-envelope')
+            ->linkToRoute('app_profil');
+
+        return $actions
+            ->add(Crud::PAGE_EDIT, $profilAction);
     }
 }
