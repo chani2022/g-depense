af1250430d73ce3b46097d250f2968c14d7c97a8
chani <chaniandrianarinaivo@gmail.com>
2025-07-08 09:04:21 +0100

    config(fix): suppression du multilangue dans easyadmin.yaml (routes)

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -40,55 +40,6 @@ class UserCrudController extends AbstractCrudController
         ];
     }
 
-    public function index(AdminContext $context)
-    {
-        $event = new BeforeCrudActionEvent($context);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        if (!$this->isGranted(Permission::EA_EXECUTE_ACTION, ['action' => Action::INDEX, 'entity' => null, 'entityFqcn' => $context->getEntity()->getFqcn()])) {
-            throw new ForbiddenActionException($context);
-        }
-
-        $fields = FieldCollection::new($this->configureFields(Crud::PAGE_INDEX));
-        $filters = $this->container->get(FilterFactory::class)->create($context->getCrud()->getFiltersConfig(), $fields, $context->getEntity());
-        $queryBuilder = $this->createIndexQueryBuilder($context->getSearch(), $context->getEntity(), $fields, $filters);
-        $paginator = $this->container->get(PaginatorFactory::class)->create($queryBuilder);
-
-        // this can happen after deleting some items and trying to return
-        // to a 'index' page that no longer exists. Redirect to the last page instead
-        if ($paginator->isOutOfRange()) {
-            return $this->redirect($this->container->get(AdminUrlGenerator::class)
-                ->set(EA::PAGE, $paginator->getLastPage())
-                ->generateUrl());
-        }
-        /** @var EntityCollection */
-        $entities = $this->container->get(EntityFactory::class)->createCollection($context->getEntity(), $paginator->getResults());
-        $this->container->get(EntityFactory::class)->processFieldsForAll($entities, $fields);
-        $procesedFields = $entities->first()?->getFields() ?? FieldCollection::new([]);
-        $context->getCrud()->setFieldAssets($this->getFieldAssets($procesedFields));
-        $actions = $this->container->get(EntityFactory::class)->processActionsForAll($entities, $context->getCrud()->getActionsConfig());
-
-        $responseParameters = $this->configureResponseParameters(KeyValueStore::new([
-            'pageName' => Crud::PAGE_INDEX,
-            'templateName' => 'crud/index',
-            'entities' => $entities,
-            'paginator' => $paginator,
-            'global_actions' => $actions->getGlobalActions(),
-            'batch_actions' => $actions->getBatchActions(),
-            'filters' => $filters,
-        ]));
-
-        $event = new AfterCrudActionEvent($context, $responseParameters);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        return $responseParameters;
-    }
 
     // public function configureCrud(Crud $crud): Crud
     // {
