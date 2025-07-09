ebf2601848c61ac4c2970e5f247b67902d22e4b2
chani <chaniandrianarinaivo@gmail.com>
2025-07-09 16:48:08 +0100

    test: echec lors d'acces au page de changement de mot de passe

--- src/Controller/ChangePasswordController.php
+++ src/Controller/ChangePasswordController.php
@@ -2,19 +2,18 @@
 
 namespace App\Controller;
 
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_USER')]
-class ChangePasswordController extends AbstractController
+class ChangePasswordController extends AbstractDashboardController
 {
     #[Route('/change/password', name: 'app_change_password')]
-    public function index(): Response
+    public function changePassword(): Response
     {
-        return $this->render('change_password/index.html.twig', [
-            'controller_name' => 'ChangePasswordController',
-        ]);
+        return $this->render('change_password/change-password.html.twig', []);
     }
 }
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 #[IsGranted('ROLE_USER')]
