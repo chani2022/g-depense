589f39da3115750b7db432125910ec3d211543db
chani <chaniandrianarinaivo@gmail.com>
2025-07-15 14:24:04 +0100

    fix(feat): affichage seulement de ses comptes salaire lors de la consultation de l'index du menu compte salaire

--- src/Controller/Admin/CompteSalaireCrudController.php
+++ src/Controller/Admin/CompteSalaireCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\CompteSalaire;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -25,4 +30,13 @@ class CompteSalaireCrudController extends AbstractCrudController
         ];
     }
     */
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $qb = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        $alias = $qb->getAllAliases()[0];
+
+        return $qb->where(sprintf('%s.owner = :user', $alias))
+            ->setParameter('user', $this->getUser());
+    }
 }
