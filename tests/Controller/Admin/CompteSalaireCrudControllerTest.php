8f31c5d95d818f3770cf814b065b47814564dc09
chani <chaniandrianarinaivo@gmail.com>
2025-07-15 14:16:46 +0100

    test: (non passant) affichage seulement de ses comptes salaire lors de la consultation de l'index du menu compte salaire

---
+++ tests/Controller/Admin/CompteSalaireCrudControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Tests\Controller\Admin;
+
+use App\Controller\Admin\CompteSalaireCrudController;
+use App\Controller\Admin\DashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Test\AbstractCrudTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use App\Tests\Trait\UserAuthenticatedTrait;
+
+final class CompteSalaireCrudControllerTest extends AbstractCrudTestCase
+{
+    use RefreshDatabaseTrait;
+    use UserAuthenticatedTrait;
+
+    protected function getControllerFqcn(): string
+    {
+        return CompteSalaireCrudController::class;
+    }
+
+    protected function getDashboardFqcn(): string
+    {
+        return DashboardController::class;
+    }
+
+    public function testIndexPageCompteSalaireAccessUserSuccessfully(): void
+    {
+        $this->simulateUserAccessPageIndexSuccessfully();
+    }
+
+    public function testIndexPageCompteSalaireAccessAdminSuccessfully(): void
+    {
+        $this->simulateAdminAccessPageIndexSuccessfully();
+    }
+
+    public function testOnlyOwnerCompteSalaireShowIfUserAuthenticated(): void
+    {
+        $this->simulateUserAccessPageIndexSuccessfully();
+        $this->assertIndexPageEntityCount(3);
+    }
+
+
+    private function simulateUserAccessPageIndexSuccessfully(): void
+    {
+        $this->client->loginUser($this->getSimpeUserAuthenticated());
+
+        $this->client->request('GET', $this->generateIndexUrl());
+        $this->assertResponseIsSuccessful();
+    }
+
+    private function simulateAdminAccessPageIndexSuccessfully(): void
+    {
+        $this->client->loginUser($this->getAdminAuthenticated());
+
+        $this->client->request('GET', $this->generateIndexUrl());
+        $this->assertResponseIsSuccessful();
+    }
+    /**
+     * @return array<array{string, string}>
+     */
+    public static function userAccessDenied(): array
+    {
+        return [
+            ['anonymous'],
+            ['roleUser']
+        ];
+    }
+}
