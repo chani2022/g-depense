f313b9260588c2b7f840e5ca61f8a4839d4a6712
chani <chaniandrianarinaivo@gmail.com>
2025-07-22 11:00:51 +0100

    test: (test non passant) ajout de controller crud pour l'entity category

---
+++ tests/Controller/Admin/Crud/Category/AbstractCategoryCrudTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Controller\Admin\Crud\Category;
+
+use App\Controller\Admin\DashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Test\AbstractCrudTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use App\Tests\Trait\UserAuthenticatedTrait;
+use Symfony\Component\DomCrawler\Crawler;
+
+abstract class AbstractCategoryCrudTest extends AbstractCrudTestCase
+{
+    use RefreshDatabaseTrait;
+    use UserAuthenticatedTrait;
+
+    protected ?Crawler $crawler;
+
+    protected function getControllerFqcn(): string
+    {
+        return CategoryController::class;
+    }
+
+    protected function getDashboardFqcn(): string
+    {
+        return DashboardController::class;
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->crawler = null;
+    }
+}
