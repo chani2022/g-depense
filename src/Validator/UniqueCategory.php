3f43556b06c91aacbf0f4438edab0aed54e16308
chani <chaniandrianarinaivo@gmail.com>
2025-07-28 08:57:29 +0100

    feat: ajout de validation pour la création de category

---
+++ src/Validator/UniqueCategory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ *
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class UniqueCategory extends Constraint
+{
+    /*
+     * Any public properties become valid options for the annotation.
+     * Then, use these in your validator class.
+     */
+    public $message = 'Catory "{{ value }}" already exist.';
+
+    #[HasNamedArguments]
+    public function __construct(
+        array $groups = null,
+        mixed $payload = null,
+    ) {
+        parent::__construct([], $groups, $payload);
+    }
+}
---
+++ src/Validator/UniqueCategoryValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Validator;
+
+use App\Repository\CategoryRepository;
+use App\Repository\CompteSalaireRepository;
+use DateTime;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueCategoryValidator extends ConstraintValidator
+{
+    public function __construct(private CategoryRepository $categoryRepository, private TokenStorageInterface $token) {}
+    /**
+     * @param DateTime $value
+     * @param DateBeetween $constraint
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        /* @var App\Validator\DateBeetween $constraint */
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if ($this->categoryRepository->getCategoryByUser($this->token->getToken()->getUser(), $value)) {
+            // TODO: implement the validation here
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $value->format('d-m-Y'))
+                ->addViolation();
+        }
+    }
+}
