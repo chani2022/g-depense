a72e218630c399dbb9a33dbe8250584ea73130a6
chani <chaniandrianarinaiv@gmail.com>
2025-08-07 14:30:55 +0200

    fix: assurer que les utilisateurs connectés sont autorisé à acceder à l'index de la page depense

--- src/Controller/Admin/DepenseCrudController.php
+++ src/Controller/Admin/DepenseCrudController.php
@@ -11,7 +11,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class DepenseCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
