e92b75c42827646bd1eadbece435932c6c90a8ab
chani <chaniandrianarinaiv@gmail.com>
2025-08-07 14:27:14 +0200

    test: (non passant) assurer que les utilisateurs connectés sont autorisé à acceder à l'index de la page depense

--- src/Controller/Admin/DepenseCrudController.php
+++ src/Controller/Admin/DepenseCrudController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
 use App\Entity\Depense;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +19,14 @@ class DepenseCrudController extends AbstractCrudController
         return Depense::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            DateTimeField::new('compteSalaire.dateDebutCompte', 'Compte du'),
+            DateTimeField::new('compteSalaire.dateFinCompte', 'Au'),
+            TextField::new('category.nom', 'Depense'),
+            TextField::new('category.prix', 'Prix'),
+            BooleanField::new('category.isVital')
         ];
     }
-    */
 }
