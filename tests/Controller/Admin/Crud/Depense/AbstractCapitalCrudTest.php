3b3f6bdc89086c1f3b4ad0e00e572686b1ad3622
chani <chaniandrianarinaiv@gmail.com>
2025-08-07 14:10:50 +0200

    feat: ajout de controller crud  pour l'entity depense

---
+++ tests/Controller/Admin/Crud/Depense/AbstractCapitalCrudTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Controller\Admin\Crud\Capital;
+
+use App\Controller\Admin\DashboardController;
+use App\Controller\Admin\DepenseCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Test\AbstractCrudTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use App\Tests\Trait\UserAuthenticatedTrait;
+use Symfony\Component\DomCrawler\Crawler;
+
+abstract class AbstractDepenseCrudTest extends AbstractCrudTestCase
+{
+    use RefreshDatabaseTrait;
+    use UserAuthenticatedTrait;
+
+    protected ?Crawler $crawler;
+
+    protected function getControllerFqcn(): string
+    {
+        return DepenseCrudController::class;
+    }
+
+    protected function getDashboardFqcn(): string
+    {
+        return DashboardController::class;
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->crawler = null;
+    }
+}
