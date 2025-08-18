a8948ff61b58c70fb476a4cd9e8319eb59ed1ce4
chani <chaniandrianarinaiv@gmail.com>
2025-08-18 10:00:11 +0200

    fix: assurer que la method build form de l'entity quantity soit appelé 2 fois

--- src/Form/QuantityType.php
+++ src/Form/QuantityType.php
@@ -4,17 +4,27 @@ namespace App\Form;
 
 use App\Entity\Quantity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class QuantityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('unite')
-            ->add('quantite')
-            ->add('owner')
+            ->add('unite', TextType::class, [
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
+            ->add('quantite', NumberType::class, [
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
         ;
     }
 
--- tests/Form/QuantityTypeTest.php
+++ tests/Form/QuantityTypeTest.php
@@ -6,6 +6,9 @@ use App\Form\QuantityType;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\FormBuilderInterface;
 use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class QuantityTypeTest extends TestCase
 {
@@ -28,14 +31,28 @@ class QuantityTypeTest extends TestCase
         /** @var MockObject|FormBuilderInterface */
         $mockFormBuilder = $this->createMock(FormBuilderInterface::class);
         $mockOptions = [];
-
+        $constraints = [
+            new NotBlank()
+        ];
         $mockFormBuilder
             ->expects($this->exactly(2))
             ->method('add')
-            ->withConsecutive([
-                ['unite'],
-                ['quantity']
-            ])
+            ->withConsecutive(
+                [
+                    'unite',
+                    TextType::class,
+                    [
+                        'constraints' => $constraints
+                    ]
+                ],
+                [
+                    'quantite',
+                    NumberType::class,
+                    [
+                        'constraints' => $constraints
+                    ]
+                ]
+            )
             ->willReturnSelf();
 
         $this->quantityType->buildForm($mockFormBuilder, $mockOptions);
