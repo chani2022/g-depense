8481e1dcce500466c29ba5fe91c10ff44cc5fb1c
chani <chaniandrianarinaiv@gmail.com>
2025-08-19 14:24:55 +0200

    chrone: ajout de symfony/ux-chartjs

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
