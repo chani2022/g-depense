39e73770b0679666d11dcead66ce77d271fe448f
chani <chaniandrianarinaiv@gmail.com>
2025-08-21 10:55:25 +0200

    test: (non passant) assurer que la dataset du chart est bien definie

--- src/Ux/ChartData.php
+++ src/Ux/ChartData.php
@@ -2,20 +2,87 @@
 
 namespace App\Ux;
 
+use App\Helper\ArrayHelper;
 use App\Repository\DepenseRepository;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ChartData
 {
+    const STYLE = [
+        'depense' => [
+            'border' => 'rgb(255, 99, 132)',
+            'background' => 'rgb(255, 99, 132)'
+        ],
+        'capital' => [
+            'border' => 'rgb(22, 157, 150)',
+            'background' => 'rgb(22, 157, 150)'
+        ],
+    ];
+
+    private array $labels = [];
+    private array $datasets = [];
+
     public function __construct(
         private DepenseRepository $depenseRepository,
-        private TokenStorageInterface $tokenStorage
+        private TokenStorageInterface $tokenStorage,
+        private ArrayHelper $arrayHelper
     ) {}
 
-    public function getLabels(?array $dates = null)
+    /**
+     * @return string[]
+     */
+    public function getLabels(): array
+    {
+        return $this->labels;
+    }
+    /**
+     * @param string[] $labels
+     * @return static
+     */
+    public function setLabels(array $labels): static
+    {
+        $this->labels =  $labels;
+
+        return $this;
+    }
+
+    public function getDatasets(): array
+    {
+        return $this->datasets;
+    }
+
+    public function setDatasets(array $datasets): static
+    {
+        $this->datasets = $datasets;
+
+        return $this;
+    }
+
+    public function getDepenses(?array $dates = null): array
     {
         $user = $this->tokenStorage->getToken()->getUser();
         $depenses = $this->depenseRepository->findDepensesWithCapital($user, $dates);
         $depensesTotal = $this->depenseRepository->getTotalDepenseAndCapitalInDateGivingByUser($user, $dates);
+
+        $mergeDepenses = $this->arrayHelper->merge($depenses, $depensesTotal);
+
+        // $depenses = $this->handleDepense($mergeDepenses);
+        return $mergeDepenses;
+    }
+
+    public function handleDepense(array $depenses): array
+    {
+        $labels = [];
+        $datasets = [];
+        foreach ($depenses as $valeur) {
+            if (is_array($valeur)) {
+                $labels[] = $valeur['label'];
+            }
+        }
+
+        return [
+            'labels' => $labels,
+            'datasets' => $datasets
+        ];
     }
 }
--- tests/Ux/ChartDataTest.php
+++ tests/Ux/ChartDataTest.php
@@ -7,6 +7,7 @@ use App\Ux\ChartData;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
 use App\Repository\DepenseRepository;
+use App\Helper\ArrayHelper;
 use DateTime;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -16,15 +17,22 @@ class ChartDataTest extends TestCase
     private ?ChartData $chartData;
     /** @var  MockObject|DepenseRepository|null */
     private $depenseRepository;
+    /** @var MockObject|ArrayHelper|null */
+    private $arrayHelper;
 
     protected function setUp(): void
     {
         /** @var  MockObject|DepenseRepository|null */
         $this->depenseRepository = $this->createMock(DepenseRepository::class);
+
+        /** @var MockObject|ArrayHelper|null */
+        $this->arrayHelper = $this->createMock(ArrayHelper::class);
+
         $tokenStorage = new TokenStorage();
         $token = new UsernamePasswordToken(new User, 'main');
         $tokenStorage->setToken($token);
-        $this->chartData = new ChartData($this->depenseRepository, $tokenStorage);
+
+        $this->chartData = new ChartData($this->depenseRepository, $tokenStorage, $this->arrayHelper);
     }
 
     protected function tearDown(): void
@@ -35,10 +43,61 @@ class ChartDataTest extends TestCase
 
     public function testGetLabels(): void
     {
-        $depenses = ['test'];
-        $depensesTotal = ['depenseTotal'];
+        $labelsActual = $this->chartData->getLabels();
 
+        $this->assertIsArray($labelsActual);
+    }
+
+    public function testSetLabels(): void
+    {
+        $this->chartData->setLabels(['label1', 'label2']);
+
+        $labelsActual = $this->chartData->getLabels();
+
+        $this->assertSame(['label1', 'label2'], $labelsActual);
+    }
+
+    public function testGetDatasets(): void
+    {
+        $datasets = $this->chartData->getDatasets();
+
+        $this->assertIsArray($datasets);
+    }
+
+    public function testSetDatasets(): void
+    {
+        $this->chartData->setDatasets(['dataset']);
+
+        $datasets = $this->chartData->getDatasets();
+
+        $this->assertSame(['dataset'], $datasets);
+    }
+
+    public function testGetDepenses(): void
+    {
+        $depenses = [
+            [
+                'id' => 1,
+                'label' => 'label',
+                'total_capital' => 15.20,
+                'total_depense' => 5.20
+            ],
+            [
+                'id' => 1,
+                'label' => 'label2',
+                'total_capital' => 15.20,
+                'total_depense' => 5.20
+            ]
+        ];
+
+        $depensesTotal = [
+            'total_depense_general' => 20.10,
+            'total_capital_general' => 100.20
+        ];
+
+        $depenseExpected = array_merge($depenses, $depensesTotal);
         $dates = [new DateTime('- 7 days'), new DateTime('+ 7 days')];
+
         $this->depenseRepository
             ->expects($this->once())
             ->method('findDepensesWithCapital')
@@ -51,9 +110,94 @@ class ChartDataTest extends TestCase
             ->with(new User, $dates)
             ->willReturn($depensesTotal);
 
-        $labelsExpected = array_merge($depenses, $depensesTotal);
-        $labelsActual = $this->chartData->getLabels($dates);
+        $this->arrayHelper
+            ->expects($this->once())
+            ->method('merge')
+            ->with($depenses, $depensesTotal)
+            ->willReturn($depenseExpected);
 
-        $this->assertSame($labelsExpected, $labelsActual);
+        $depenseActual = $this->chartData->getDepenses($dates);
+
+        $this->assertSame($depenseExpected, $depenseActual);
+    }
+    /**
+     * @dataProvider provideDepenses
+     */
+    public function testHandleDepense(array $depense, array $expected): void
+    {
+        $dataChart = $this->chartData->handleDepense($depense);
+
+        $this->assertSame($expected, $dataChart);
+    }
+
+    public static function provideDepenses(): array
+    {
+        $depenses = [
+            'total_depense_general' => 20.10,
+            'total_capital_general' => 100.20,
+            [
+                'id' => 1,
+                'label' => 'label',
+                'total_capital' => 15.20,
+                'total_depense' => 5.20
+            ],
+            [
+                'id' => 1,
+                'label' => 'label2',
+                'total_capital' => 15.20,
+                'total_depense' => 5.20
+            ]
+        ];
+        return [
+            [
+                'only label' => [
+                    'test' => 1,
+                    'test2' => 2,
+                    [
+                        'label' => 'label1'
+                    ],
+                    [
+                        'label' => 'label2'
+                    ]
+                ],
+                'expected' => [
+                    'labels' => ['label1', 'label2'],
+                    'datasets' => []
+                ]
+            ],
+            [
+                'only datasets' => [
+                    'total_depense_general' => 20.10,
+                    'total_capital_general' => 100.20,
+                    [
+                        'id' => 1,
+                        'total_capital' => 15.20,
+                        'total_depense' => 5.20
+                    ],
+                    [
+                        'id' => 1,
+                        'total_capital' => 100.20,
+                        'total_depense' => 8
+                    ]
+                ],
+                'expected' => [
+                    'labels' => [],
+                    'datasets' => [
+                        [
+                            'label' => 'depense',
+                            'data' => [5.20, 8],
+                            'borderColor' => ChartData::STYLE['depense']['border'],
+                            'backgroundColr' => ChartData::STYLE['depense']['background'],
+                        ],
+                        [
+                            'label' => 'capital',
+                            'data' => [15.20, 100.20],
+                            'borderColor' => ChartData::STYLE['capital']['border'],
+                            'backgroundColr' => ChartData::STYLE['capital']['background'],
+                        ],
+                    ]
+                ]
+            ]
+        ];
     }
 }
